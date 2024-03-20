28d51654da39a062b905e20b276aaaf8941640ae
murphyyue <murphyyue@icloud.com>
2024-03-20 15:12:20 +0800

    add introduce

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import Intro from '@/components/intro';
+
 export default function Home() {
   return (
     <main className="flex flex-col items-center px-4">
-      hello
+      <Intro />
     </main>
   );
 }
