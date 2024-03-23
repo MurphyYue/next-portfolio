441e67845cf21c90846fb18fc1ef79e6e29a127b
murphyyue <murphyyue@icloud.com>
2024-03-23 17:01:56 +0800

    add my projects

---
+++ components/projects.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import React from "react";
+import Project from "./project";
+import SectionHeading from "./section-heading";
+import { projectsData } from "@/lib/data";
+import { useSectionInView } from "@/lib/hooks";
+
+export default function Projects() {
+  const { ref } = useSectionInView("Projects");
+  return (
+    <section ref={ref} id="projects" className="scroll-mt-28 mb-28">
+      <SectionHeading>My projects</SectionHeading>
+      <div>
+        {projectsData.map((project, index) => (
+          <React.Fragment key={index}>
+            <Project {...project} />
+          </React.Fragment>
+        ))}
+      </div>
+    </section>
+  );
+}
--- lib/data.ts
+++ lib/data.ts
@@ -1,3 +1,5 @@
+import projectDemoImg from "@/public/peoject-demo.png";
+
 export const links = [
   {
     name: "Home",
@@ -22,5 +24,29 @@ export const links = [
   {
     name: "Contact",
     hash: "#contact",
+  },
+] as const;
+
+export const projectsData = [
+  {
+    title: "Intelligent Finance&Taxation Management platform",
+    description:
+      "I worked as a front-end developer on this project for 2 years. Users from small companies can manage their enterprise's finance and tax on this platform",
+    tags: ["React", "mobx", "eggjs", "typescript", "webpack", "dojojs"],
+    imageUrl: projectDemoImg,
+  },
+  {
+    title: "Yongyou AIoT Management",
+    description:
+      "This project is used to connect the equipment in the user's factory, better monitor the operation of the equipment, and remind the relevant personnel to deal with the equipment when there is something wrong with the equipment.",
+    tags: ["React", "react-router", "react-redux", "ant-design", "webpack", "echarts"],
+    imageUrl: projectDemoImg,
+  },
+  {
+    title: "Investment Management platform",
+    description:
+      "An Investment Management platform for Longfor Group which include PC and mobile(iOS by H5)",
+    tags: ["qiankun", "vue", "vuex", "vue-router", "elementUI", "echarts"],
+    imageUrl: projectDemoImg,
   },
 ] as const;
