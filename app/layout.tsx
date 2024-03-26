b1596f8a22baba5e2085e5e150be9c5f5e8d8e27
murphyyue <murphyyue@icloud.com>
2024-03-26 15:47:45 +0800

    add theme switch

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import "./globals.css";
 import ActiveSectionContextProvider from "@/context/active-section-context";
 import Header from "@/components/header";
 import { Toaster } from "react-hot-toast";
+import ThemeContextProvider from "@/context/theme-context";
+import ThemeSwitch from "@/components/theme-switch";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,11 +24,15 @@ export default function RootLayout({
       <body
         className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
       >
-        <ActiveSectionContextProvider>
-          <Header />
-          {children}
-          <Toaster position="top-right" />
-        </ActiveSectionContextProvider>
+
+        <ThemeContextProvider>
+          <ActiveSectionContextProvider>
+            <Header />
+            {children}
+            <Toaster position="top-right" />
+            <ThemeSwitch />
+          </ActiveSectionContextProvider>
+        </ThemeContextProvider>
       </body>
     </html>
   );
